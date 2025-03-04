4d6225d2a66a25adb1e0f442d67ce3822cdbefdf
이초이 <choi1223@hunet.co.kr>
2025-03-04 17:47:17 +0900

    sample

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,15 +14,15 @@ export default function Index() {
   return (
     <div className="container px-4 py-8 mx-auto">
       <div className="grid grid-cols-2 gap-2">
-        <Link to='/' className="flex items-center gap-1 px-3 py-6 break-all rounded-lg border border-[#f5f7ff] transition hover:shadow-md">
+        <Link to='/rank' className="flex items-center gap-1 px-3 py-6 break-all rounded-lg border border-[#f5f7ff] transition hover:shadow-md">
           <ChartBarIcon className="text-orange-400 size-6 shrink-0"/>
           <strong>음원 순위</strong>
         </Link>
-        <Link to='/' className="flex items-center gap-1 px-3 py-6 break-all rounded-lg border border-[#f5f7ff] transition hover:shadow-md">
+        <Link to='/guide' className="flex items-center gap-1 px-3 py-6 break-all rounded-lg border border-[#f5f7ff] transition hover:shadow-md">
           <BookmarkIcon className="text-orange-400 size-6 shrink-0"/>
           <strong>스밍/투표 가이드</strong>
         </Link>
-        <Link to='/' className="flex items-center col-span-2 gap-1 px-3 py-6 break-all rounded-lg border border-[#f5f7ff] transition hover:shadow-md">
+        <Link to='/community' className="flex items-center col-span-2 gap-1 px-3 py-6 break-all rounded-lg border border-[#f5f7ff] transition hover:shadow-md">
           <ChatBubbleBottomCenterIcon className="text-orange-400 size-6 shrink-0"/>
           <strong>헬퍼 지원</strong>
         </Link>
